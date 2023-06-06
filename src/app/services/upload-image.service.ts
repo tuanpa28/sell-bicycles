7b4f41f2bca514a510adf7eb19b905baf7ba48f6
Namykuuu <128971809+vchien2003@users.noreply.github.com>
2023-06-06 15:58:20 +0700

    feat: add image refs: AW-2

---
+++ src/app/services/upload-image.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadImageService {
+
+  constructor(private http: HttpClient) { }
+  uploadImage(imageFile: File): Observable<any> {
+    const formData = new FormData();
+    formData.append('image', imageFile);
+
+    return this.http.post('https://asmbe.vercel.app/api/images/upload', formData);
+  }
+
+}
