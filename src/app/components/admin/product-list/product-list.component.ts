fb29cf91de75eb5f8e7b28b05f722665ebee6bb6
Namykuuu <128971809+vchien2003@users.noreply.github.com>
2023-06-06 17:40:57 +0700

    feat: list image in admin products refs: AW-10

--- src/app/components/admin/product-list/product-list.component.ts
+++ src/app/components/admin/product-list/product-list.component.ts
@@ -12,9 +12,16 @@ export class ProductListComponent {
 
   constructor(private productService: ProductService) {
     this.productService.getProducts().subscribe(
-      ({products}) => (this.products = products.data),
+      ({ products }) => (this.products = products.data),
       (error) => console.log(error)
     );
+  }
+
+  getProductImageUrl(image: object): string {
+    if (image && typeof image === 'object' && 'url' in image) {
+      return (image as any).url;
+    }
+    return ''; // Default or fallback image URL
   }
 
   onHandleRemove(id: any) {
